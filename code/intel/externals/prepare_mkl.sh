56eeb2865dfae01017f7cbbabdcae219ebee718f
Roman Dubtsov <roman.s.dubtsov@intel.com>
2017-05-12 10:59:45 +0300

    MKL: clean up prepare_mkl.sh

--- code/intel/externals/prepare_mkl.sh
+++ code/intel/externals/prepare_mkl.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 # set -ex
 
-FindLibrary() 
-{
-  LOCALMKL=`find $1 -name libmklml_intel.so`   # name of MKL lib
-}
-
 # Check what MKL lib we have in MKLROOT
-if [ -z `find $MKLROOT -name libmkl_rt.so -print -quit` ]; then
+if [ -z "$MKLROOT" -o -z `find $MKLROOT -name libmkl_rt.so -print -quit` ]; then
   # mkl_rt has not been found; we are dealing with MKLML
-
-  if [ -z $LOCALMKL ] ; then
+  if [ -z "$LOCALMKL" ] ; then
     # LOCALMKL is not set, when MKLROOT was set manually and it points to MKLML in correct version
-    FindLibrary $MKLROOT
+    LOCALMKL=`find $1 -name libmklml_intel.so`   # name of MKL lib
   fi
 
-  LIBRARIES=`basename $LOCALMKL | sed -e 's/^.*lib//' | sed -e 's/\.so.*$//'`
+  if [ -n "$LOCALMKL" ] ; then
+    LIBRARIES=`basename $LOCALMKL | sed -e 's/^.*lib//' | sed -e 's/\.so.*$//'`
+    MKLROOT="$LOCALMKL"
+  fi
 else
   LIBRARIES="mkl_rt"
-fi 
+fi
 
 echo $MKLROOT $LIBRARIES
